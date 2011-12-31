df944109e9ab88dba96564a75602d5a67d918679
Ralf <ralf@ranulf.net>
2011-12-31 16:33:48 +0000

    now that i know the stack works, i might as well use it...

--- codegen/testrom.asm
+++ codegen/testrom.asm
@@ -1,16 +1,17 @@
 
 	org #0000
 
-start:
-
-
 	ld ix,0
+	ld sp,#fffe
+	ld a,'#'
+	call outchar
 
+start:
 	ld hl,#8000
-	xor a
-fill:	ld (hl),a
-	inc l
-	cp l
+fill:	ld (hl),#bd
+	inc hl
+	ld a,l
+	or h
 	jr nz, fill
 
 repeat:	
@@ -20,223 +21,65 @@ loop:
 	ld a,(hl)
 	or a
 	jr z,restart
-
-	ld bc,#fadd
-busy1:	in a,(c)
-	rla
-	jr nc,busy1
-	dec c
-
-	ld a,(hl)
-	out (c),a
-
+	call outchar
 	inc hl
 	jr loop
 
 restart:
-	inc c
-busy2:	in a,(c)
-	rla
-	jr nc,busy2
-	dec c
-
+	ld a,ixh
+	call outhex
 	ld a,ixl
-
-	rra
-	rra
-	rra
-	rra
-
-	and #f
-	add a,#90
-	daa
-	adc a,#40
-	daa
-	out (c),a
-
-	inc c
-busy3:	in a,(c)
-	rla
-	jr nc,busy3
-	dec c
-
-	ld a,ixl
-
-	and #f
-	add a,#90
-	daa
-	adc a,#40
-	daa
-	out (c),a
+	call outhex
 
 domodify:
 	ld b,ixl
-
-	ld hl,#8000
+modify_loop2:
+	ld c,b
+	ld hl,#4000
 modify_loop:
 	inc (hl)
 	inc hl
 	djnz modify_loop
+	ld b,c
+	djnz modify_loop2
 
 	inc ix
 
 	ld hl,#3000
-	ld de,#d0a
 
 dump_memory:
 	ld bc,#fade
 	out (c),l
 
-	ld bc,#fadc
-
 	ld a,l
 	and #f
 	jr nz,no_header
 
-	inc c
-busy4:	in a,(c)
-	rla
-	jr nc,busy4
-	dec c
-	out (c),d
-
-	inc c
-busy5:	in a,(c)
-	rla
-	jr nc,busy5
-	dec c
-	out (c),e
-
-	inc c
-busy6:	in a,(c)
-	rla
-	jr nc,busy6
-	dec c
-
-
-; output hight byte of address
+	ld a,#d
+	call outchar
+	ld a,#a
+	call outchar
 
 	ld a,h
-	rra
-	rra
-	rra
-	rra
-
-	and #f
-	add a,#90
-	daa
-	adc a,#40
-	daa
-	out (c),a
-
-	inc c
-busy7h:	in a,(c)
-	rla
-	jr nc,busy7h
-	dec c
-
-	ld a,h
-	and #f
-	add a,#90
-	daa
-	adc a,#40
-	daa
-	out (c),a
-
-	inc c
-busy8l:	in a,(c)
-	rla
-	jr nc,busy8l
-	dec c
-
-; output low byte of address
-
+	call outhex
 	ld a,l
-	rra
-	rra
-	rra
-	rra
-
-	and #f
-	add a,#90
-	daa
-	adc a,#40
-	daa
-	out (c),a
-
-	inc c
-busy7:	in a,(c)
-	rla
-	jr nc,busy7
-	dec c
-
-	ld a,'0'
-	out (c),a
-
-	inc c
-busy8:	in a,(c)
-	rla
-	jr nc,busy8
-	dec c
-
-; continue with output
+	call outhex
 
 	ld a,':'
-	out (c),a
+	call outchar
 
 no_header:
-
-	inc c
-busy9:	in a,(c)
-	rla
-	jr nc,busy9
-	dec c
-
 	ld a,' '
-	out (c),a
-
-	inc c
-busya:	in a,(c)
-	rla
-	jr nc,busya
-	dec c
+	call outchar
 
 	ld a,(hl)
+	call outhex
 
-	rra
-	rra
-	rra
-	rra
-
-	and #f
-	add a,#90
-	daa
-	adc a,#40
-	daa
-	out (c),a
-
-	inc c
-busyb:	in a,(c)
-	rla
-	jr nc,busyb
-	dec c
-
-
-	ld a,(hl)
-
-	and #f
-	add a,#90
-	daa
-	adc a,#40
-	daa
-	out (c),a
 donext:
 	inc hl
 	ld a,l
 	or h
 	jp nz, dump_memory
-
-	; output a counter
 
 	; jp repeat
 
@@ -246,7 +89,41 @@ donext:
 	push af
 	call repeat
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; output hex in A, corrupts AF
 
+outhex:
+	push af
+	rra
+	rra
+	rra
+	rra
+	call outhexnibble
+	pop af
+outhexnibble:
+	and #f
+	add a,#90
+	daa
+	adc a,#40
+	daa
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; output character in A
+
+outchar:
+	push bc
+outcharbusy:
+	ld bc,#fadd
+	in c,(c)
+	rl c
+	jr nc,outcharbusy
+
+	ld bc,#fadc
+	out (c),a
+	pop bc
+	ret
 
 string:
-       defb 13,10,"Data dump - ",0
+       defb 13,10,"Data dump, iteration ",0
