35f792476d61de262e3bd39a215e87093181ad3b
Ralf <ralf@ranulf.net>
2012-01-05 08:29:39 +0000

    optimise SPI loader code so it fits into 32 bytes amd has all the ED bytes aligned on even bytes

--- codegen/spiload.asm
+++ codegen/spiload.asm
@@ -1,32 +1,31 @@
 
 	org #0000
 
-; transfer 16K of data from #7c000 on memory chip
+; transfer 16K of data from #7c000 on memory chip to #8000 and jump to it
+
+	ld hl,#8000			; transfer address
 
 	ld bc,#fafe			; FAFE = SPI control
 	out (c),c			; drive bit 0 low = flash ROM chip select
 
-	ld hl,#4000			; transfer address
 	inc c				; FAFF = SPI data port
+	ld de,#03c0			; D = SPI: READ, E = middle byte of address
 
-	ld a,#03			; SPI: READ
-	out (c),a
+	out (c),d			; 03 = read
+	out (c),c			; FF = MSB addr
+	out (c),e			; C0 = mid addr
+	out (c),l			; 00 = LSB addr -> output 3 bytes of address FFC000 (top 16KB)
 	
-	ld de,#7c0			; transfer from #7c000, address is D:E:L (L=0)
-	out (c),d
-	out (c),e
-	out (c),l			; output 3 bytes of address
-	
-	ld a,#80			; end at #8000
 xferloop:
-	out (c),a			; dummy write to trigger a read
+	out (c),l			; dummy write to trigger a read
 	ini				; read the byte and store it in memory
 	inc b				; fix b which has just been decremented
-	cp h
+	ld a,h				; end at #c000
+	cp e
 	jr z,xferloop			; loop until we reach the last byte
 
-	ld a,c				; FF
-	dec c				; FAFE = SPI control
-	out (c),a			; turn off ROM chip select
+;	ld a,c				; FF
+;	dec c				; FAFE = SPI control
+;	out (c),a			; turn off ROM chip select
 
-	jp #4000			; finally, execute the boot program
+	jp #8000			; finally, execute the boot program
---
+++ hdl/spiload.vhd
@@ -0,0 +1,57 @@
+-- spiload.vhd
+library IEEE;
+use ieee.std_logic_1164.all;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use ieee.std_logic_arith.all;
+
+entity spiload is
+    port(
+        addr        : in  std_logic_vector(13 downto 0);
+        data        : out std_logic_vector(7 downto 0)
+    );
+end spiload;
+
+architecture impl of spiload is
+
+begin
+process (addr)
+    begin
+        case addr(4 downto 0) is 
+            when "00000" => data <= X"21";
+            when "00001" => data <= X"00";
+            when "00010" => data <= X"80";
+            when "00011" => data <= X"01";
+            when "00100" => data <= X"fe";
+            when "00101" => data <= X"fa";
+            when "00110" => data <= X"ed";
+            when "00111" => data <= X"49";
+            when "01000" => data <= X"0c";
+            when "01001" => data <= X"11";
+            when "01010" => data <= X"c0";
+            when "01011" => data <= X"03";
+            when "01100" => data <= X"ed";
+            when "01101" => data <= X"51";
+            when "01110" => data <= X"ed";
+            when "01111" => data <= X"49";
+            when "10000" => data <= X"ed";
+            when "10001" => data <= X"59";
+            when "10010" => data <= X"ed";
+            when "10011" => data <= X"69";
+            when "10100" => data <= X"ed";
+            when "10101" => data <= X"69";
+            when "10110" => data <= X"ed";
+            when "10111" => data <= X"a2";
+            when "11000" => data <= X"04";
+            when "11001" => data <= X"7c";
+            when "11010" => data <= X"bb";
+            when "11011" => data <= X"28";
+            when "11100" => data <= X"f7";
+            when "11101" => data <= X"c3";
+            when "11110" => data <= X"00";
+            when "11111" => data <= X"80";
+
+            when others => data <= X"00";
+          end case;
+    end process;
+
+end impl;
