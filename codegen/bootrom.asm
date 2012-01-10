739bf04752896e312a5d6cbc4e868a7247ca4419
Ralf <ralf@ranulf.net>
2012-01-10 09:26:03 +0000

    bootrom still a bit iffy

--- codegen/bootrom.asm
+++ codegen/bootrom.asm
@@ -1,5 +1,5 @@
 
-	org #0000
+	org #0080
 
 ; transfer 16K of data from #7c000 on memory chip to #c000 and jump to it
 
@@ -12,11 +12,36 @@
 	ld c,#dc
 	out (c),a
 
+	ld bc,#feff
+	out (c),c				; ensure SPI bus is idle
+
+	sbc hl,hl
+delay:	dec hl
+	ld a,h
+	or l
+	jr nz,delay				; wait a bit
+
+	ld a,#ab
+	out (c),b				; turn on flash rom CE
+	inc b					; change to SPI data port
+	out (c),a				; wake up
+	out (c),a				; dummy
+	out (c),a				; dummy
+	out (c),a				; dummy
+	out (c),a				; dummy
+	dec b
+	out (c),b
+
+delay2:	dec hl
+	ld a,h
+	or l
+	jr nz,delay2				; wait a bit
+
 	ld de,#0307				; D=READ
 	ld hl,#c000				; EHL = transfer address
 
-	ld bc,#feff
-	out (c),c				; ensure SPI bus is idle
+	ld sp,hl
+	push hl
 
 	out (c),b				; turn on flash rom CE
 	inc b					; change to SPI data port
@@ -25,11 +50,14 @@
 	out (c),h				; addr 2
 	out (c),l				; addr 3
 
-	dec hl	;	in a,(c)		; dummy read
+	in a,(c)				; dummy read
+	;dec hl	;	in a,(c)		; dummy read
 xferloop:
 	in a,(c)
 	ld (hl),a
 	inc hl
+
+	call printhex
 
 	ld a,h
 	or l
@@ -38,4 +66,37 @@ xferloop:
 	dec b
 	out (c),c				; finish with SPI bus
 
-	jp #c000
+	ret	;jp #c000
+
+
+printhex:
+	push af
+	rra
+	rra
+	rra
+	rra
+	and #f					; upper nibble in A
+	cp 10
+	sbc a,#69
+	daa					; A=hex character in ascii
+	call putch
+	pop af
+	and #f					; lower nibble in A
+	cp 10
+	sbc a,#69
+	daa					; A=hex character in ascii
+
+; output char in A
+
+putch:
+	push bc
+	ld bc,#fadd
+putchloop:
+	defb #ed,#70	;in f,(c)
+	jp p,putchloop				; loop until tx uart idle
+	dec c
+	out (c),a
+	pop bc
+	ret
+
+
