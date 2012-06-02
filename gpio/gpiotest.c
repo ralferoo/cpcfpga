2f5f2717836cffee099025b8c912a751871158fb
Ranulf Doswell <ralf@ranulf.net>
2012-06-02 16:35:23 +0100

    prom dump

--- gpio/gpiotest.c
+++ gpio/gpiotest.c
@@ -182,6 +182,8 @@ inline int pinInput(int i)
 	return ( GPIO_LEV & (1<<i) ) ? 1 : 0;
 }
 
+///////////////////////////////////////////////////////////////////////////
+
 unsigned long long tot_nanos = 0;
 
 inline void nsleep(long nanos)
@@ -202,6 +204,8 @@ inline void nsleep(long nanos)
 	}
 #endif
 }
+
+///////////////////////////////////////////////////////////////////////////
 
 inline void jtagPulseClock(void)
 {
@@ -472,6 +476,44 @@ uint32_t jtagSendDR( uint32_t value, int len, struct Device *device)
 	return jtagShiftData(value,len,device->hdr,device->tdr);
 }
 
+void jtagUpdateOrIdle(void)
+{
+	switch (jtag_state) {
+	default:
+		jtagReset();
+
+	case JTAG_STATE_RESET:
+		jtagOutputSilent(0,0);
+
+        case JTAG_STATE_UPDATE:
+        case JTAG_STATE_IDLE:
+		break;
+
+        case JTAG_STATE_SELECT_DR:
+		jtagOutputSilent(0,1);
+
+        case JTAG_STATE_SELECT_IR:
+		jtagOutputSilent(0,1);
+		jtagOutputSilent(0,0);
+		break;
+
+        case JTAG_STATE_CAPTURE:
+        case JTAG_STATE_SHIFT:
+        case JTAG_STATE_PAUSE:
+		jtagOutputSilent(0,1);
+
+        case JTAG_STATE_EXIT1:
+        case JTAG_STATE_EXIT2:
+		jtagOutputSilent(0,1);
+                break;
+	}
+		
+	if (jtag_state != JTAG_STATE_UPDATE && jtag_state != JTAG_STATE_IDLE) {
+		printf("Invalid state transitioning to IDLE or UPDATE: %s\n", get_jtag_state_name() );
+		exit(1);
+	}
+}
+
 void jtagRunTestTCK( unsigned int i )
 {
 	jtagIdle();
@@ -511,6 +553,8 @@ void jtagScanDR(void)
 		printf("\n");
 	}
 }
+
+///////////////////////////////////////////////////////////////////////////
 
 void devFreeDevices(void)
 {
@@ -667,6 +711,43 @@ void devScanDevices(void)
 	}
 }
 
+///////////////////////////////////////////////////////////////////////////
+
+static uint8_t hexXsum;
+
+inline void hexByte( uint8_t byte )
+{
+        printf("%02X", byte);
+        hexXsum -= byte;
+}
+
+inline void hexEndLine(void)
+{
+        printf("%02X\n", hexXsum);
+}
+
+void hexStart( uint8_t type, uint16_t faddr, uint8_t len )
+{
+        printf(":%02X%04X%02X", len, faddr, type);
+        hexXsum = 0 - len - (faddr>>8) - (faddr&0xff) - type;
+}
+
+void hexAddrHigh( uint16_t hiaddr )
+{
+        hexStart( 4, 0, 2 );
+        hexByte( hiaddr>>8 );
+        hexByte( hiaddr & 0xff );
+        hexEndLine();
+}
+
+void hexEndOfFile()
+{
+        hexStart( 1, 0, 0 );
+        hexEndLine();
+}
+
+///////////////////////////////////////////////////////////////////////////
+
 void promValidate(struct Device* prom)
 {
 	// idcode
@@ -688,6 +769,86 @@ void promValidate(struct Device* prom)
 	}
 	printf("IR status register: %02x\n", protect);
 }
+
+void promDumpBlock( int faddr, struct Device *device)
+{
+	int i, j, bit;
+
+        // ISC_ADDRESS_SHIFT faddr instruction
+        jtagSendIR(0xeb, device);
+        jtagSendDR(faddr, 16, device);
+        jtagRunTestTCK(2);
+
+        // ISC_READ fvfy0 instruction
+        jtagSendIR(0xef, device);
+        jtagRunTestTCK(1);
+        jtagRunTestTCK(50);
+
+        // read the data
+        jtagShiftDR();
+        for (i=0; i<device->hdr; i++)
+                jtagOutputSilent(0,0);              // ignore all data before the data we want
+
+        uint16_t addr = faddr << 4;
+        for (i=0; i<8192; i+=32*8 ) {
+                hexStart( 0, addr, 32 );
+                for(j=0; j<32; j++ ) {
+                        uint8_t byte = 0;
+                        for(bit = 0; bit<8; bit++) {
+                                byte >>= 1;
+                		if (jtagOutputSilent(0,0) )
+                                        byte |= 0x80;
+                        }
+                        hexByte( byte );
+                        addr++;
+                }
+                hexEndLine();
+        }
+
+	jtagUpdateOrIdle();
+}
+
+void promDump(struct Device* prom)
+{
+	promValidate(prom);
+
+        // bypass instruction
+        jtagSendIR( 0xff, prom);
+        // ISC_ENABLE ispen instruction
+        jtagSendIR( 0xe8, prom);
+        jtagSendDR( 0x34, 6, prom);
+        // ISC_DISABLE conld instruction
+        jtagSendIR( 0xf0, prom);
+        jtagRunTestTCK(110000);
+        // ISC_ENABLE ispen instruction
+        jtagSendIR( 0xe8, prom);
+        jtagSendDR( 0x34, 6, prom);
+
+        uint16_t faddr, faddr_base = (uint16_t) ~0U;
+        for( faddr=0x0000; faddr<0x4000; faddr+=0x40 ) {
+		printf("\n");
+                printf( "# faddr=%04X\n", faddr );
+                if ( (faddr & 0xf000) != faddr_base ) {
+                        faddr_base = faddr & 0xf000;
+                        hexAddrHigh( faddr >> 12 );
+                }
+                promDumpBlock(faddr, prom);
+        }
+        hexEndOfFile();
+
+	// ISC_DISABLE conld
+	jtagSendIR(0xf0, prom);
+	jtagRunTestTCK(110000);
+
+	int protect = (int) jtagSendIR(0xff, prom);
+	if ( (protect&7) != 1 ) {
+		printf("IR status register not as expected: %02x\n", protect);
+		exit(1);
+	}
+	printf("IR status register: %02x\n", protect);
+}
+
+///////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char **argv)
 {
@@ -714,6 +875,7 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 	promValidate(prom);
+	promDump(prom);
 
 //	printf("Should have been %lld nanos, %lld usec, %lld ms\n", tot_nanos, tot_nanos/1000, tot_nanos/1000000);
 	exit(0);
