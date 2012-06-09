947765a08ce759e3a4bda9d223585af47103623d
Ranulf Doswell <ralf@ranulf.net>
2012-06-09 19:59:39 +0100

    so, a few more shorts but none of them major

--- gpio/pintest.c
+++ gpio/pintest.c
@@ -122,9 +122,10 @@ void dump_dr_stream(const char* name, char* dr_stream, int totdr)
 	printf("\n");
 }
 
-void diff_dr_stream(const char* name, char* dr_stream, char* dr2_stream, int totdr, struct Device *fpga, struct Device *prom)
+void diff_dr_stream(const char* name, const char* ignore, char* dr_stream, char* dr2_stream, int totdr, struct Device *fpga, struct Device *prom)
 {
-	printf("%s:\n", name);
+	if (name)
+		printf("%s:\n", name);
 	int d;
 	for (d=0; d<2; d++) {
 		struct Device* device = d ? fpga : prom;
@@ -135,20 +136,22 @@ void diff_dr_stream(const char* name, char* dr_stream, char* dr2_stream, int tot
 			if (dr_stream[pos] != dr2_stream[pos] ) {
 				char* port = cells->port ? cells->port : "???";
 
-				char* type = "???";
-				switch(cells->function) {
-					case BC_F_INTERNAL: type="internal";	break;
-					case BC_F_INPUT	  : type="input";	break;
-					case BC_F_OUTPUT3 : type="output3";	break;
-					case BC_F_CONTROLR: type="controlr";	continue; //break;
+				if( strcmp(port, "IO_P56") && ( ignore==0 || strcmp(port,ignore) ) ) {
+					char* type = "???";
+					switch(cells->function) {
+						case BC_F_INTERNAL: type="internal";	break;
+						case BC_F_INPUT	  : type="input";	break;
+						case BC_F_OUTPUT3 : type="output3";	break;
+						case BC_F_CONTROLR: type="controlr";	continue; //break;
+					}
+					printf("%10s %3d %-8s (%3d) %20s %d %d\n",
+						device->name, cells->cellnum, type, pos, port,
+						dr_stream[pos], dr2_stream[pos] );
 				}
-				printf("%10s %3d %-8s (%3d) %20s %d %d\n", device->name, cells->cellnum, type, pos, port,
-					dr_stream[pos], dr2_stream[pos] );
 
 			}
 		}
 	}
-	printf("\n");
 }
 
 void copy_safe_bits(char* safe_dr, int totdr, struct Device *device, struct BoundaryScan *cells)
@@ -216,6 +219,72 @@ void test_candidate_pins(char* safe_dr, int totdr, struct Device *fpga, struct D
 	}
 	printf("\n");
 }
+
+void real_test_pin( struct Device *device, struct BoundaryScan* cells,
+		char* safe_dr, int totdr, char* test_dr, char* test2_dr, char* test3_dr,
+		struct Device *fpga, struct Device *prom)
+{
+	memcpy(test_dr, safe_dr, totdr);
+
+	int pos=device->user + cells->cellnum;
+	int ctrl = cells->control >= 0 ? cells->control + device->user : -1;
+
+	printf("Testing pin %s on %s, %3d (%3d) control %3d (%3d) DC=%d:\n", cells->port, device->name,
+		cells->cellnum, pos, cells->control, ctrl, cells->disable_control);
+
+	if (ctrl >= 0 && (cells->disable_control==0 || cells->disable_control==1) )
+		test_dr[ctrl] = 1-cells->disable_control;
+
+	test_dr[pos] = 1;
+
+	nsleep(5000);
+	send_dr_stream(test_dr, totdr, test2_dr);			// send the initial test vector
+	nsleep(1000);
+	send_dr_stream(test_dr, totdr, test3_dr);			// send it again just to be sure
+	nsleep(1000);
+	send_dr_stream(test_dr, totdr, test3_dr);			// send it again just to be sure
+
+	int i;
+	for (i=0; i<8; i++) {
+		if (i&2) {
+			test_dr[pos] = 1;
+		} else {
+			test_dr[pos] = 0;
+		}
+
+		nsleep(1000);
+		if (i&1) 						// alternate results between test2 and test3
+			send_dr_stream(test_dr, totdr, test3_dr);
+		else
+			send_dr_stream(test_dr, totdr, test2_dr);
+
+		diff_dr_stream(0, cells->port, test2_dr, test3_dr, totdr, fpga, prom);
+	}
+}
+
+void test_pin( struct Device *device, char* pin, char* safe_dr, int totdr, char* test_dr, char* test2_dr, char* test3_dr,
+		struct Device *fpga, struct Device *prom)
+{
+	struct BoundaryScan* cells;
+	if (device == prom)
+		cells = XCF02S_VO20_BSCAN;
+	else if (device == fpga)
+		cells = XC3S400_TQ144_BSCAN;
+	else {
+		printf("Can't test pin %s as device isn't FPGA or PROM!\n", pin);
+		return;
+	}
+
+	for (;cells->cellnum >= 0; cells++) {
+		if( cells->function == BC_F_OUTPUT3 && cells->port && strcmp(cells->port, pin)==0 ) {
+			real_test_pin( device, cells, safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+			return;
+		}
+	}
+
+	printf("Can't find output pin %s to test in device %s!\n", pin, device->name);
+}
+
 
 void pintest(void)
 {
@@ -297,19 +366,19 @@ void pintest(void)
 	send_dr_stream(safe_dr, totdr, test2_dr);
 //	dump_dr_stream("test2_dr during safe", test2_dr, totdr);
 
-	diff_dr_stream("Differences from test_dr and test2_dr", test_dr, test2_dr, totdr, fpga, prom);
+	diff_dr_stream("Differences from test_dr and test2_dr", 0, test_dr, test2_dr, totdr, fpga, prom);
 
 	nsleep(1000);
 	send_dr_stream(safe_dr, totdr, test3_dr);
 //	dump_dr_stream("test3_dr during safe", test3_dr, totdr);
 
-	diff_dr_stream("Differences from test2_dr and test3_dr", test2_dr, test3_dr, totdr, fpga, prom);
+	diff_dr_stream("Differences from test2_dr and test3_dr", 0, test2_dr, test3_dr, totdr, fpga, prom);
 
 	nsleep(1000);
 	send_dr_stream(safe_dr, totdr, test4_dr);
 //	dump_dr_stream("test4_dr during safe", test4_dr, totdr);
 
-	diff_dr_stream("Differences from test3_dr and test4_dr", test3_dr, test4_dr, totdr, fpga, prom);
+	diff_dr_stream("Differences from test3_dr and test4_dr", 0, test3_dr, test4_dr, totdr, fpga, prom);
 
 	nsleep(1000);
 	send_dr_stream(initial_dr, totdr, test_dr);
@@ -317,7 +386,130 @@ void pintest(void)
 
 	test_candidate_pins(initial_dr, totdr, fpga, prom);
 
-//	test_pin( fpga, "IO_P1", safe_dr, test_dr, test2_dr, test3_dr, fpga, prom );
+	// ram address lines
+	test_pin( fpga, "IO_P5", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P24", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P23", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P21", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P20", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P18", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P17", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P15", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P14", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P4", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P2", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P35", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P1", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P13", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P5", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P12", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P8", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P11", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P7", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P10", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+
+	// ram control lines
+	test_pin( fpga, "IO_P36", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P6", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+
+	// video
+	test_pin( fpga, "IO_P63", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P60", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P55", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P53", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P59", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P57", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P52", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+
+	test_pin( fpga, "IO_P68", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P69", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+
+	// tape and exp control lines
+	test_pin( fpga, "IO_P131", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P47", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P50", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P51", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+
+	test_pin( fpga, "IO_P76", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P74", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P77", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+
+	// gpio
+	test_pin( fpga, "IO_P137", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P135", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P132", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P130", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P129", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P127", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P128", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P123", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+
+	// leds
+	test_pin( fpga, "IO_P85", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P83", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P84", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P86", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P87", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+
+	// joy0
+	test_pin( fpga, "IO_P118", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P116", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P119", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P113", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P112", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P122", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+
+	// joy1
+	test_pin( fpga, "IO_P105", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P104", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P107", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P103", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P102", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P108", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+
+	// sd slot
+	test_pin( fpga, "IO_P40", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P140", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P141", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P41", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+
+	// ps2
+	test_pin( fpga, "IO_P46", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P45", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+
+	// extclk
+	test_pin( fpga, "IO_P100", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+
+	// avr
+	test_pin( fpga, "IO_P98", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P99", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P125", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P97", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P96", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P95", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P93", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P92", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P90", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P89", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+
+	// flash
+	test_pin( fpga, "IO_P79", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P73", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P82", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P80", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P70", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+	test_pin( fpga, "IO_P78", safe_dr, totdr, test_dr, test2_dr, test3_dr, fpga, prom );
+
+	// possible shorts:
+	//	P1,P2,P4 x lots - FIXED
+	//
+	//	P131,P140 occasional (casin, sd_di)
+	//	P118,119,122 (down, fire2, up on joy0)
+	//	P113,116,118 (left, fire1, down on joy0)
+	//	P122, P123 (up, gpio10)
+	//	P59, P57 (video, ok)
+	//	P53, P55 (video, ok)
+	//	P60, P63 (video, ok)
 
 	send_ir_stream(bypass_ir, totir);
 
